310aad7769dbd79ef24a130bb8fb6b744c9ef891
GunaSheelan A <130417487+Gunasheelan16@users.noreply.github.com>
2023-05-25 16:44:14 +0530

    script.js

--- script.js
+++ script.js
@@ -1,15 +1,63 @@
-// a. let obj1 = { name:"Person 1", age:5 };
-// b. let obj2 = { age:5, name:"Person 1" };
+// 1.To Compare Two JSON have the same property without order
 
-let obj1 = { name:"Person 1", age:5 };
-let obj2 = { age:5, name:"Person 1" };
+// let obj1 = {"name":"person1","age":5};
+// let obj2 = {"age":5,"name":"person1"};
 
-let str1 = JSON.stringify(obj1);
-let str2 = JSON.stringify(obj2);
+// var Json=true;
+// if(Object.keys(obj1).length==Object.keys(obj2).length){
+//     for(key in obj1) { 
+//         if(obj1[key] == obj2[key]) {
+//             continue;
+//         }
+//         else {
+//             Json=false;
+//             break;
+//         }
+//     }
+// }
+// else {
+//     obje=false;
+// }
+// console.log("is object equal"+Json);
 
-if (str1 === str2) {
-    console.log("Objects are equal");
-} 
-else {
-    console.log("Objects are not equal");
-}
+
+
+// let request = new XMLHttpRequest();
+// request.addEventListener('load',printData);
+// request.open("GET", "https://restcountries.com/v3.1/all");
+// request.send();
+// console.log(request);
+
+// 1.Print flag
+// function printData() {
+//   const countries = JSON.parse(this.responseText);
+//   countries.forEach((coun) => console.log(coun.flags.png) );
+// }
+
+// 2. To Print official name
+
+// function printData() {
+//   const countries = JSON.parse(this.responseText);
+//   countries.forEach((coun) => console.log(coun.name.official) );
+// }
+
+// 3.To Print Region
+
+// function printData() {
+//     const countries = JSON.parse(this.responseText);
+//     countries.forEach((coun) => console.log(coun.region) );
+//   }
+
+//4.To Print Subregion
+
+// function printData() {
+//     const countries = JSON.parse(this.responseText);
+//     countries.forEach((coun) => console.log(coun.subregion) );
+//   }
+
+//5.To print all population 
+ 
+// function printData() {
+//     const countries = JSON.parse(this.responseText);
+//     countries.forEach((coun) => console.log(coun.population) );
+//   }
